42fd744f45d13c10de933547d6693b323d563494
Markus Maal <mina@markusmaal.ee>
2026-05-24 17:46:51 +0300

    Test commit * Rename "build" workflow to "test" (GitHub) ~ DummyTest intentionally fails

--- FlipnicLib/Formats/Dummy.cs
+++ FlipnicLib/Formats/Dummy.cs
@@ -4,6 +4,6 @@ namespace FlipnicLib.Formats;
 public class Dummy(Stream stream) : FormatBase
 {
     private readonly long _size = stream.Length;
-    private readonly string _zeroPad = stream.ReadBytes((int)stream.Length).Sum(b => b) == 0 ? "Yes" : "No"; // the value is "Yes" if every byte is 00
+    private readonly string _zeroPad = "Yes" ; // the value is "Yes" if every byte is 00
     public override string ToString() => $"Dummy file\n\nPurpose: Prevent system crash when DVD laser tries to read past the game data\nZero padded: {_zeroPad}\nTotal size: {GetFilesizeString(_size)}";
 }
